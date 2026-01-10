6b6ded76a6204742b77cb7b4020addc508be5953
YashGoyal1523 <yashgoyal1523@gmail.com>
2026-01-10 19:52:45 +0530

    Initial

--- 23_binary tree/2no_of_nodes.cpp
+++ 23_binary tree/2no_of_nodes.cpp
@@ -7,8 +7,8 @@ void countnodes(TreeNode* root, int &count){
         return;
     }
     count++;
-    countnodes(root->left);
-    countnodes(root->right);
+    countnodes(root->left,count);
+    countnodes(root->right,count);
 
 }
 
@@ -16,4 +16,29 @@ int NoOfNodes(TreeNode* root){
     int count=0;
     countnodes(root,count);
     return count;
+}
+
+// other method
+
+//nodes=1(count of current node) + nodes(left subtree) + nodes(right subtree)
+
+int countnodes(TreeNode* root){
+   
+    if(root==NULL){
+        return 0;
+    }
+    
+    int left=countnodes(root->left);
+    int right=countnodes(root->right);
+
+    int totalnodes=left+right+1;
+    
+    return totalnodes;
+
+}
+
+int NoOfNodes(TreeNode* root){
+    
+    int ans=countnodes(root);
+    return return;
 }
