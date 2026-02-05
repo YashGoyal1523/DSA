2df6f32c28b160996b8923a163fc7c2b44c6bccc
YashGoyal1523 <yashgoyal1523@gmail.com>
2026-02-05 12:04:36 +0530

    Initial

--- 34Learning/3techniques/19rotation_by_k.cpp
+++ 34Learning/3techniques/19rotation_by_k.cpp
@@ -1,7 +1,7 @@
 //array of size n
 
 //rotate right by k
-// new index= (index+k)%n (k<n hona chahie ...k=k%n krlena)
+// new index= (index+k)%n 
 
 //rotate left by k
 // new index = index-k>=0?index-k:index-k+n (k<n hona chahie ...k=k%n krlena)
