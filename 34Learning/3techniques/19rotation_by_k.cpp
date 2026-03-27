55f5aaa03727d62e20d3cde024886d842e181bfa
Yash Goyal <yashgoyal1523@gmail.com>
2026-03-27 13:36:11 +0530

    Initial

--- 34Learning/3techniques/19rotation_by_k.cpp
+++ 34Learning/3techniques/19rotation_by_k.cpp
@@ -1,9 +1,9 @@
 //array of size n
 
 //rotate right by k
-// new index= (index+k)%n   (agar index+k overflow kre toh k%n krlena)
+// new index= (index+k)%n   (k<n ki esi koi need nhi hai....toh k=k%n is optional ......agar index+k overflow kre toh k%n krlena)
 
 //rotate left by k
 // new index = index-k>=0?index-k:index-k+n (k<n hona chahie ...k=k%n krlena)
 //or
-// new index= (index - k + n)%n (k<n hona chahie (mathematically no , programitcally yes) ...k=k%n krlena)
+// new index= (index - k + n)%n (k<n hona chahie ...k=k%n krlena)
