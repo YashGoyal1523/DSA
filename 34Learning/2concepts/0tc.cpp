d7562e085c01a163e08506447420d07ab1dedf17
Yash Goyal <yashgoyal1523@gmail.com>
2026-03-30 11:00:29 +0530

    Initial

--- 34Learning/2concepts/0tc.cpp
+++ 34Learning/2concepts/0tc.cpp
@@ -5,3 +5,5 @@
 // n<=10^6 -> o(nlogn)
 // n<=10^8 -> o(n)
 // n>10^8 -> o(logn) or o(1)
+
+// tip : ques jab o(n) mein krna ho...toh approach sochne ke liye iss way mein bhi dekh skte ki har index pe kuch processing krni hai 
