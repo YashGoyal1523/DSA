a64b9cc05ca9990de93a0179ca7a6b82ef168bdc
Yash Goyal <yashgoyal1523@gmail.com>
2026-03-30 17:06:27 +0530

    initial

---
+++ 34Learning/3techniques/0ques_approaches.cpp
@@ -0,0 +1,12 @@
+// 1. Remove from ends ⇒ Think “KEEP a subarray”
+
+// If problem says:
+
+// remove from left/right
+// minimize operations
+
+// Always try:
+
+// Find longest subarray satisfying condition
+// Answer = n - length
+
