bdcbd638e79a1433345502f30fabf633d7448ac1
Yash Goyal <yashgoyal1523@gmail.com>
2026-04-08 12:44:36 +0530

    Initial Commit

---
+++ 25QUES/1mean_data_stream.cpp
@@ -0,0 +1,9 @@
+// heap ka ques nhi h ... bas saath rkhne k liye
+
+
+// stream of data h ... instance ka mean nikalna h
+
+//approach
+//bruteforce - numbers ki list rkho ... jab bhi chahie sum nikalke mean nikalte jao : not good ... har instance pe O(n) lagega
+//better - total sum and total count maintain rkhte jao ... har instance ka mean = total sum/total count
+
