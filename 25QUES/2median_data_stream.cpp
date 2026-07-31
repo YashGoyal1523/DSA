7b4e470cde4270aab50fefc6288466e26e9aa394
Yash Goyal <yashgoyal1523@gmail.com>
2026-08-01 01:20:58 +0530

    Initial

--- 25QUES/2median_data_stream.cpp
+++ 25QUES/2median_data_stream.cpp
@@ -1,90 +1,74 @@
+// stream of data h ... instance ka mean nikalna h
+
+//approach
+//bruteforce - numbers ki list rkho ... jab bhi chahie sum nikalke mean nikalte jao : not good ... har instance pe O(n) lagega
+//better - total sum and total count maintain rkhte jao ... har instance ka mean = total sum/total count
+
 //lc 295
 
 //stream of data h ...  instance ka median nikalna h
+// median - ascending order mein (n elements) : n/2+1 th element (if n is odd) , n/2 th and n/2+1 th element ka avg (if n is even)
+
 
 //bruteforce-numbers ki list rkho ... jab bhi chahie sort krke median nikalte jao : not good ... har instance pe O(nlogn) lagega
 
 //better
-// sorted order ka first part max heap m rkho , second half min heap m rkho
-// ( even elements case):
-//    1 2 3 4 5 6
-//    -max- -min-
-//  size of max heap=size of min heap -> median = max ka top and min ka top ka avg
+// https://youtu.be/jnj87BSi9Is?si=ypquy4B9P7LOvNnB
+//(O(logn) insertion using priority_queue
+//T.C : The overall time complexity is O(log N) for the addNum method and O(1) for the findMedian method.
+//S.C : O(N).
 
-// (odd elements case):
-//  1 2 3  4 5
-//  -max- -min-
-// size of max heap=size of min heap +1 -> median = max ka top 
-//   1 2  3 4 5
-//  -max- -min-
-//  size of min heap=size of max heap +1 -> median = min ka top 
+//ek max heap for left half, ek min heap for right half
+////always maintain left_max_heap size one greater than rigght_min_heap size
+//ya fir, dono ka size equal ho
 
+//on num coming
+//if left heap is empty or number is <= top of max heap add to left heap
+// else add to right heap
+//agar right mein ek zyada hojaye toh top left m daaldena
+//similarly agar left mein >1 zyada hojaye toh top right m daaldena
 
-// algo
-// ek max heap maxi banao, ek min heap mini banao 
-// initialise median=0;
-//  element aaya :
-// if maxi size ==  mini size : then if element>median toh push in mini and median= min ka top (size of min heap=size of max heap +1) ....else push in maxi and median= max ka top (size of max heap=size of min heap +1) 
-// else if maxi size== mini size +1 : then if element>median toh push in mini ....else maxi ka top mini mein daaldo fir element maxi mein and median= min ka top and max ke top ka avg (size of max heap=size of min heap)
-// else ( mini size== maxi size +1 ) :  if element>median toh mini ka top maxi m daaldo fir element mini mein ...else maxi m push krdo element and median= min ka top and max ke top ka avg (size of max heap=size of min heap)
-
+//if size equal then dono ke top ka avg is median 
+// if left max heap size is more then uska top
 
 
 class MedianFinder {
 public:
-    
-    priority_queue<int> maxi;
-	priority_queue<int,vector<int>, greater<int> > mini;
-    double median;
+    priority_queue<int> left_max_heap; //max heap
+    priority_queue<int, vector<int>, greater<int>> right_min_heap; //min heap
     MedianFinder() {
-        median=0;
+        
     }
     
     void addNum(int num) {
-        if(maxi.size() == mini.size()) {
-		if(num > median) {
-			mini.push(num);
-			median = mini.top();
-		}
-		else {
-			maxi.push(num);
-			median = maxi.top();
-		}
-	}
-	else if(maxi.size() == mini.size()+1) {
-		if(num > median) {
-			mini.push(num);
-		}
-		else {
-			int maxTop = maxi.top();
-			maxi.pop();
-			mini.push(maxTop);
-			maxi.push(num);
-		}
-		median = (mini.top() + maxi.top())/2.0;
-	}
-	else if(mini.size() == maxi.size()+1) {
-		if(num > median) {
-			int minTop = mini.top();
-			mini.pop();
-			maxi.push(minTop);
-			mini.push(num);
-		}
-		else {
-			maxi.push(num);
-		}
-		median = (mini.top() + maxi.top())/2.0;
-	}
+        if(left_max_heap.empty() || num <= left_max_heap.top()) {
+            left_max_heap.push(num);
+        } else {
+            right_min_heap.push(num);
+        }
+        
+        
+        //always maintain left_max_heap size one greater than rigfht_min_heap size
+        //ya fir, dono ka size equal ho
+        
+        if(left_max_heap.size()>right_min_heap.size()+1) { // note: .size() returns unsigned int toh isiliye yaha left_max_heap.size()-right_min_heap.size()>1 mat krna as agar case hua 1-2 toh voh -1 na hoke unsgned ke karan wrap hoke large positive banke yeh true hojayega
+            right_min_heap.push(left_max_heap.top());
+            left_max_heap.pop();
+        } else if(left_max_heap.size() < right_min_heap.size()) {
+            left_max_heap.push(right_min_heap.top());
+            right_min_heap.pop();
+        }
+        
     }
     
     double findMedian() {
-        return median;
+        if(left_max_heap.size() == right_min_heap.size()) {
+            // matlab even number of elements hue honge
+            
+            return (double)(left_max_heap.top()+right_min_heap.top())/2;
+        }
+        
+        //else hamare paas odd number of elemenes hue honge
+        return left_max_heap.top();
     }
 };
-
-/**
- * Your MedianFinder object will be instantiated and called as such:
- * MedianFinder* obj = new MedianFinder();
- * obj->addNum(num);
- * double param_2 = obj->findMedian();
- */
