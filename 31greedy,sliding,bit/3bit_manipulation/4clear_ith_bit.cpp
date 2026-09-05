26e84396debf3c4dacafdec7471d4e139afcc3bd
Yash Goyal <yashgoyal1523@gmail.com>
2026-09-06 02:20:34 +0530

    initial

--- 31greedy,sliding,bit/3bit_manipulation/4clear_ith_bit.cpp
+++ 31greedy,sliding,bit/3bit_manipulation/4clear_ith_bit.cpp
@@ -1,8 +1,6 @@
 // clear(make 0) ith bit
 
 // masking
-// m1 mask wale(sab zero,ith wala 1) se xor krdo
-//1<<i se banjayega
 
 //  mask wala(sab 1,ith wala 0) se & krdo - yeh wla mask banane ke liye ..... ~(1<<i) se hojayega
 // initially voh bit 0 ho ya 1 dono case sambhal jaayeege  and finally 0 hi milega
