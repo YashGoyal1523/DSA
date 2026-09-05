4a115ecbb765c56a81d1ca99c51cce38fb0b3165
Yash Goyal <yashgoyal1523@gmail.com>
2026-09-06 02:36:21 +0530

    initial

--- 31greedy,sliding,bit/3bit_manipulation/12no_of_jumps.cpp
+++ 31greedy,sliding,bit/3bit_manipulation/12no_of_jumps.cpp
@@ -1,3 +1,3 @@
-// given a staircase of n . jumps are allowed in steps of 2^k . min jumps to reach topmost  step from ground? 
+// given a staircase of n . jumps are allowed in steps of 2^k . min jumps to reach topmost step from ground? 
 
-// ANS- no of set bits in n gives the min no of jumps 
+// ANS- no of set bits in n gives the min no of jumps .....because To minimize the number of jumps, we want to use the fewest powers of 2....binary representation of n already gives exactly that representation.
