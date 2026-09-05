7bb376e508bc138a7d43fbd953c913e36fd5ac21
Yash Goyal <yashgoyal1523@gmail.com>
2026-09-06 02:30:48 +0530

    initial

--- 31greedy,sliding,bit/3bit_manipulation/12no_of_jumps.cpp
+++ 31greedy,sliding,bit/3bit_manipulation/12no_of_jumps.cpp
@@ -1,3 +1,3 @@
-// given a staircase of n . jumps are allowed in steps of 2^k . min jumps to reach topmost  step from ground? (destination is always reachable)
+// given a staircase of n . jumps are allowed in steps of 2^k . min jumps to reach topmost  step from ground? 
 
 // ANS- no of set bits in n gives the min no of jumps 
