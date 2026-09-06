1f4ac35ee2e86d7c0cc74427ea2bcdc3114769ac
Yash Goyal <yashgoyal1523@gmail.com>
2026-09-06 10:41:18 +0530

    initial

--- 5arrays/23majority_element.cpp
+++ 5arrays/23majority_element.cpp
@@ -19,12 +19,11 @@
 
 // Explanation:
 // Algorithm:
-
 // Initialize two variables: count and candidate. Set count to 0 and candidate to an arbitrary value.
 // Iterate through the array nums:
-// a. If count is 0, assign the current element as the new candidate and increment count by 1.
-// b. If the current element is the same as the candidate, increment count by 1.
-// c. If the current element is different from the candidate, decrement count by 1.
+// a. If count is 0, assign the current element as the new candidate and take count as 1.
+// b. else If the current element is the same as the candidate, increment count by 1.
+// c. else (the current element is different from the candidate) decrement count by 1.
 // After the iteration, the candidate variable will hold the majority element.
 
 // class Solution {
