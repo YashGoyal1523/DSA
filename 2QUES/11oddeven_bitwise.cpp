5dbaefa915daf0bbe4a8c0b8f1ee03e08033d1ca
Yash Goyal <yashgoyal1523@gmail.com>
2026-09-24 18:22:04 +0530

    initial

--- 2QUES/11oddeven_bitwise.cpp
+++ 2QUES/11oddeven_bitwise.cpp
@@ -1,10 +1,10 @@
-// normal methd :  if %2==0 then even if %2==1 then odd
+// normal methd :  if %2==0 (rem on div by 2 == 0) then even if %2==1 (rem on div by 2 ==1) then odd
 
 #include<iostream>       //bitwise method  // if Lsb is 0 ie rightmost bit is 0 then no is even and if 1 then odd
 using namespace std ;
 
 int check(int n){
-    int x=n&1;
+    int x=n&1; // note: %2 wale ko bhi lsb check krna bol skte
     if (x==0)
     {
         return 0;       // lets take return 0 for even and return 1 fpr odd
